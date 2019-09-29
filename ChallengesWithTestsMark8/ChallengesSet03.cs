938cc8ef9b7401283336be86ea38027652c041d3
Hunter Elam <hunterelam21@gmail.com>
2019-09-28 22:37:38 -0500

    Week 3 completed

--- ChallengesWithTestsMark8/ChallengesSet03.cs
+++ ChallengesWithTestsMark8/ChallengesSet03.cs
@@ -7,43 +7,69 @@ namespace ChallengesWithTestsMark8
     {
         public bool ArrayContainsAFalse(bool[] vals)
         {
-            foreach(bool val in vals)
+            for(int i = 0; i < vals.Length; i++)
             {
-                if(val == false)
-                { 
+                if(vals[i] == true)
+                {
+                    vals[i] = vals[i];
+                }
+                else
+                {
                     return true;
                 }
             }
+            return false;
         }
 
         public bool IsSumOfOddsOdd(IEnumerable<int> numbers)
         {
-            int sumofOdds = 0;
+            int Sum = 0;
             foreach(int num in numbers)
             {
                 if(num % 2 == 1)
                 {
-                    sumofOdds += num;
-                }
-                else
-                {
-                    sumofOdds += 0;
-                }
-
-                if (sumofOdds % 2 == 1)
-                {
-                    return true;
-                }
-                else
-                {
-                    return false;
+                    Sum += num;
                 }
             }
+            if(Sum % 2 == 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
         }
 
         public bool PasswordContainsUpperLowerAndNumber(string password)
         {
-            throw new NotImplementedException();
+            int ctr = 0;
+            string up = password.ToUpper();
+            string low = password.ToLower();
+            string num = "0123456789";
+
+            foreach(char u in password)
+            {
+                if(up[u] == password[u])
+                {
+                    ctr++;
+                }
+                if(low[u] == password[u])
+                {
+                    ctr++;
+                }
+                if(num[u] == password[u])
+                {
+                    ctr++;
+                }
+            }
+
+            if(ctr == 3)
+            {
+                return true;
+            }
+            else { return false; }
         }
 
         public char GetFirstLetterOfString(string val)
@@ -69,16 +95,16 @@ namespace ChallengesWithTestsMark8
         public int[] GetOddsBelow100()
         {
             int[] odds = {
-                1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25,
-                27, 29, 31, 33, 35, 37, 39, 41, 43, 45, 47, 49,
-                51, 53, 55, 57, 59, 61, 63, 65, 67, 69, 71, 73,
-                75, 77, 79, 81, 83, 85, 87, 89, 91, 93, 95, 97, 99};
+                1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, //13
+                27, 29, 31, 33, 35, 37, 39, 41, 43, 45, 47, 49, //12
+                51, 53, 55, 57, 59, 61, 63, 65, 67, 69, 71, 73, //12
+                75, 77, 79, 81, 83, 85, 87, 89, 91, 93, 95, 97, 99}; //13
             return odds;
         }
 
         public void ChangeAllElementsToUppercase(string[] words)
         {
-            throw new NotImplementedException();
+            words = Array.ConvertAll(words, d => d.ToUpper());
         }
     }
 }
